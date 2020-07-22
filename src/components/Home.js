935fd28a79ff10643eabaf413b78b3876b87326b
pharia-le <pharia.le@gmail.com>
2020-07-21 20:56:19 -0700

    Update imports, add Login Component to render() & handleSuccessfulAuth.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import axios from "axios";
+
 import Registration from './auth/Registration'
+import Login from './auth/Login'
 
 class Home extends Component {
     constructor(props) {
@@ -7,9 +10,20 @@ class Home extends Component {
         
     }
 
-    handleSuccessfulAuth(data) {
+    handleSuccessfulAuth = data => {
         this.props.handleLogin(data);
         this.props.history.push("/dashboard");
+    }
+    
+    handleLogoutClick = () => {
+        axios
+            .delete("http://localhost:3000/logout", { withCredentials: true })
+            .then(response => {
+                this.props.handleLogout();
+            })
+            .catch(error => {
+                console.log("logout error", error);
+            });
     }
     
     render() {
@@ -17,7 +31,9 @@ class Home extends Component {
             <div>
                 <h1>Home</h1>
                 <h1>Status: {this.props.loggedInStatus}</h1>
+                <button onClick={() => this.handleLogoutClick()}>Logout</button>
                 <Registration handleSuccessfulAuth={this.handleSuccessfulAuth} />
+                <Login handleSuccessfulAuth={this.handleSuccessfulAuth} />
             </div>
         );
     }
